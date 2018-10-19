260a80c7ffa33686d39e95e8c9736155e21b2031
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2018-10-19 12:00:29 -0400

    daily dave of work

--- Python/Hangman.py
+++ Python/Hangman.py
@@ -1,3 +1,4 @@
+
 #Python Challenge - Hangman
 #Written by Kaylee and Gabby
 
@@ -5,13 +6,13 @@ turns = 0
 
 word = input("Player 1, what is the word")
 print("\n" * 50)
-print("i just need an array with " + str(len(word)) + " blanks in it.")
+#print("i just need an array with " + str(len(word)) + " blanks in it.")
 
 guess = input("Player 2, guess a letter")
 guessniw = []
 guessiw = []
 
-if letter in word:
+for letter in word:
     guessiw.append("_")
 
 def printguy(t):
@@ -66,19 +67,24 @@ while turns < 8:
         turns += 1
         guessniw.append(guess)
     else:
-        print("You got a letter")    
+        print("You got a letter")
+        
+    for index,letter in enumerate(word):
+        if letter == guess:
+            guessiw[index] = guess
     printguy(turns)
     print(guessniw)
     print(guessiw)
     guess = input("Player 2, guess another letter")
 
-def guessiw():
+if guessiw == word:
+    print("You got the word! Cool Bro!")
+
+    
+if turns == 8:
+    print("GAME OVER!! You LOST ha ha ha. Go cry in a hole. ")
+
     
 
 
-for letter in word:
-    for index,letter in enumerate(word):
-        if letter == guess:
-            guessiw[index] = guess
-    guessiw.append(guess)        
 
