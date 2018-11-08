1994ddc927538b35d9778efdd9c872e97b8add71
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2018-11-08 12:34:39 -0500

    Daily save of work

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -4,15 +4,21 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+#GPIO.setup(27,GPIO.OUT)
 
 app = Flask(__name__)
+
+GPIO.output(17,GPIO.LOW)
 
 @app.route("/", methods=["GET","POST"])
 def index():
 	if request.method == "POST":
+		#GPIO.output(27, GPIO.HIGH)
 		GPIO.output(17,GPIO.HIGH)
 		msg = request.form.get("submitBtn")
+
 	else:
+		#GPIO.output(27,GPIO.LOW)
 		GPIO.output(17,GPIO.LOW)
 		msg = "No click yet."
 	return render_template("index.html",msg=msg)
