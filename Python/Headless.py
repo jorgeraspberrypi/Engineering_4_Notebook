9c9a79bd5a565507d9e2e3d5399fbf0036ef45e0
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2019-01-17 11:01:57 -0500

    Daily save of work

--- Python/Headless.py
+++ Python/Headless.py
@@ -28,12 +28,12 @@ outline = 1
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-draw.rectangle((0,0, width, height), outline=0, fill=0)
+draw = ImageDraw.Draw(image)
+draw.rectangle((0,0, width, height), outline = 0, fill =0)
 padding = 2
 shape_width = 50
 top = padding
 bottom = height-padding
-draw = ImageDraw.Draw(image)
 x = padding
 font = ImageFont.load_default()
 
@@ -44,14 +44,14 @@ while True:
 	accel, mag = lsm303.read()
 	accel_x, y, z = accel
 	mag_x, my, mz = mag
-	mystrx = 'x = ' + str(abs(accel_x/100))
-	print('Accel X {0}, Mag X={1}'.format(accel_x, mag_x))
-	time.sleep(0.5)
+	#mystrx = 'x = ' + str(abs(accel_x/100))
+	#print('Accel X {0}, Mag X={1}'.format(accel_x, mag_x))
+	#time.sleep(0.5)
 
-	draw.text((x, top),     'Accel data', font=font, fill=255)
-	draw.text((x, top+20), mystrx, font=font, fill=255)
+	#draw.text((x, top),     'Accel data', font=font, fill=255)
+	#draw.text((x, top+20), mystrx, font=font, fill=255)
 	draw.rectangle((0,0,width,height), outline=0, fill=0)
-	draw.rectangle((0,0,128,int(accel_x0/10), outline=0, fill=100)	
+	draw.rectangle((0,0,128,int(accel_x)/10), outline=0, fill=100)	
 
 
 	disp.image(image)
