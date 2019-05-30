916fa9861cbefd439df9c8cc72d5a0c343c71cd1
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2019-05-30 12:32:41 -0400

    Commiting updates

--- Python/PIintheSky.py
+++ Python/PIintheSky.py
@@ -1,14 +1,18 @@
 import time
 import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
 import Adafruit_LSM303
+import RPi.GPIO as GPIO
+from time import sleep
 
-print('Printing accelerometer & x, y, z axis values. press Ctrl-c to quit...')
+lsm303 = Adafruit_LSM303.LSM303()
+
 while True:
-accel, mag = lsm303.read()
-    accel_x, accel_y, accel_z = accel
-    mag_x, mag_y, mag_z = mag
-    mystrx = 'x = ' + str(abs(accel_x/100))
-    mystry = 'y = ' + str(accel_y/100)
-    mystrz = 'z = ' + str(accel_z/100)
-                         
+	accel, mag = lsm303.read()
+	accel_x, accel_y, accel_z = accel
+	mag_x, mag_y, mag_z = mag
+	mystrx = 'x = ' + str(abs(accel_x/100))
+	mystry = 'y = ' + str(accel_y/100)
+	mystrz = 'z = ' + str(accel_z/100)
+	print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
+          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+	time.sleep(0.5)
