fe2b60961657866232afacbb8d18ad386ec88089
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2019-06-04 12:50:25 -0400

    Commiting Pi in the sky code

--- Python/PIintheSky.py
+++ Python/PIintheSky.py
@@ -6,7 +6,13 @@ from time import sleep
 
 lsm303 = Adafruit_LSM303.LSM303()
 
-while True:
+f = open("flightdata.py","w+")
+counter = 0
+# runs on crontab
+# maybe a delay?
+# turn on an LED or something
+while counter < 40:
+	time.sleep(120)
 	accel, mag = lsm303.read()
 	accel_x, accel_y, accel_z = accel
 	mag_x, mag_y, mag_z = mag
@@ -16,3 +22,6 @@ while True:
 	print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
 	time.sleep(0.5)
+	counter += 1
+	f.write(mystrx)
+f.close()
