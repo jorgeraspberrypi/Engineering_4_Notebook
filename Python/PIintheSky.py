addffeea80cd564e39b8f425dcd9b06e1033215b
Kaylee&Gabby <gwade51@charlottesvilleschools.org>
2019-06-06 11:14:55 -0400

    commiting final code

--- Python/PIintheSky.py
+++ Python/PIintheSky.py
@@ -3,16 +3,23 @@ import Adafruit_GPIO.SPI as SPI
 import Adafruit_LSM303
 import RPi.GPIO as GPIO
 from time import sleep
+import datetime
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
 
 lsm303 = Adafruit_LSM303.LSM303()
 
-f = open("flightdata.py","w+")
+filename = "flightdata" + str(datetime.datetime.now()) + ".txt"
+f = open(filename,"+w")
 counter = 0
-# runs on crontab
-# maybe a delay?
-# turn on an LED or something
+# runs at startup with crontab with backup fligh data file named error.txt
+# delay start
+time.sleep(120)
+# turn on LED to show start of data collection
+GPIO.output(18,GPIO.HIGH)
 while counter < 40:
-	time.sleep(120)
 	accel, mag = lsm303.read()
 	accel_x, accel_y, accel_z = accel
 	mag_x, mag_y, mag_z = mag
@@ -24,4 +31,6 @@ while counter < 40:
 	time.sleep(0.5)
 	counter += 1
 	f.write(mystrx)
+	f.write("\n")
 f.close()
+GPIO.output(18,GPIO.LOW)
